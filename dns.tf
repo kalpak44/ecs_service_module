f5f1f60e365c3ec475cf24ab76485db2813cc0b7
Pavel Usanli <pau@foryouandyourcustomers.com>
2022-03-02 18:11:09 +0200

    enable/disable public and private accessibility

--- dns.tf
+++ dns.tf
@@ -8,6 +8,7 @@ resource "aws_route53_record" "public" {
     zone_id                = data.aws_lb.public_lb.zone_id
     evaluate_target_health = true
   }
+  depends_on = [data.aws_lb.public_lb]
 }
 resource "aws_route53_record" "private" {
   count   = var.private_load_balancer_name == null ? 0 : 1
@@ -19,4 +20,5 @@ resource "aws_route53_record" "private" {
     zone_id                = data.aws_lb.private_lb.zone_id
     evaluate_target_health = true
   }
+  depends_on = [data.aws_lb.private_lb]
 }
