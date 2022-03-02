01800941e21bb20188613c1321fef547770fa386
Pavel Usanli <pau@foryouandyourcustomers.com>
2022-03-02 19:17:46 +0200

    fix dns

--- dns.tf
+++ dns.tf
@@ -3,10 +3,13 @@ resource "aws_route53_record" "public" {
   zone_id = data.aws_route53_zone.public_zone[0].id
   name    = "${var.subdomain}.${var.public_domain}"
   type    = "A"
-  alias {
-    name                   = data.aws_lb.public_lb[0].dns_name
-    zone_id                = data.aws_lb.public_lb[0].zone_id
-    evaluate_target_health = true
+  dynamic "alias" {
+    for_each = var.public_load_balancer_name == null ? [] : [1]
+    content {
+      name                   = data.aws_lb.public_lb[0].dns_name
+      zone_id                = data.aws_lb.public_lb[0].zone_id
+      evaluate_target_health = true
+    }
   }
 }
 resource "aws_route53_record" "private" {
@@ -14,9 +17,12 @@ resource "aws_route53_record" "private" {
   zone_id = data.aws_route53_zone.private_zone[0].id
   name    = "${var.subdomain}.${var.private_domain}"
   type    = "A"
-  alias {
-    name                   = data.aws_lb.private_lb[0].dns_name
-    zone_id                = data.aws_lb.private_lb[0].zone_id
-    evaluate_target_health = true
+  dynamic "alias" {
+    for_each = var.private_load_balancer_name == null ? [] : [1]
+    content {
+      name                   = data.aws_lb.private_lb[0].dns_name
+      zone_id                = data.aws_lb.private_lb[0].zone_id
+      evaluate_target_health = true
+    }
   }
 }
