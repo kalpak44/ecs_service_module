8905e78e4b86029b20ca54edc8d083e57506b086
Pavel Usanli <pau@foryouandyourcustomers.com>
2022-03-02 17:29:51 +0200

    enable/disable public and private accessibility

--- alb.tf
+++ alb.tf
@@ -3,6 +3,7 @@ resource "aws_lb_listener_rule" "public_listener_rule" {
   listener_arn = data.aws_lb_listener.listener_rule_on_public_lb_443.arn
   priority     = var.load_rule_priority
   action {
+    count        = var.public_load_balancer_name == null ? 1 : 0
     type             = "forward"
     target_group_arn = aws_lb_target_group.public_lb_target_group.arn
   }
@@ -26,6 +27,7 @@ resource "aws_lb_listener_rule" "private_listener_rule" {
   listener_arn = data.aws_lb_listener.listener_rule_on_private_lb_80.arn
   priority     = var.load_rule_priority
   action {
+    count        = var.private_load_balancer_name ? 1 : 0
     type             = "forward"
     target_group_arn = aws_lb_target_group.private_lb_target_group.arn
   }
